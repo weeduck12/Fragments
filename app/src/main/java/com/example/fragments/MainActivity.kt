5165c37a1672a22eb6620f7892a3193087ca8942
SAKHO Bouna <bouna.sakho@geii2.iut-cachan.u-psud.fr>
2025-05-19 17:32:30 +0200

    changements

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val boutonpage1 : Button = findViewById(R.id.button)
+        val boutonpage2 : Button = findViewById(R.id.button2)
+
+        val fragment1 : Fragment1 = Fragment1()
+        val fragment2 : Fragment2 = Fragment2()
+
+        boutonpage1.setOnClickListener {
+            remplaceFragment(fragment1)
+        }
+        boutonpage2.setOnClickListener{
+            remplaceFragment(fragment2)
+        }
+    }
+
+    private fun remplaceFragment(fragment : Fragment)
+    {
+        val fragMan : FragmentManager = supportFragmentManager
+        val fragTran = fragMan.beginTransaction()
+        fragTran.replace(R.id.fragmentContainerView, fragment)
+        fragTran.commit()
     }
 }
